f34fdcaff70c934fc733d8281e67a5a7b0299550
tuananhnp <45874177+tuananhnp@users.noreply.github.com>
2024-02-22 02:07:39 +0700

    Update 1122e42b671a7793@756.js

--- 1122e42b671a7793@756.js
+++ 1122e42b671a7793@756.js
@@ -15,7 +15,7 @@ async function _image(imgfile,DOM,$0)
   let img = await imgfile.image();
   let aspect = img.width / img.height;
   let height = 480;
-  let width = height / aspect;
+  let width = height * aspect;
   let canvas = DOM.canvas(width, height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0, width, height);
