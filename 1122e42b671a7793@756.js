b843c1999670e5a68a9bcb1adb51be94c175f1fe
tuananhnp <45874177+tuananhnp@users.noreply.github.com>
2024-02-21 15:21:02 +0700

    Update 1122e42b671a7793@756.js

--- 1122e42b671a7793@756.js
+++ 1122e42b671a7793@756.js
@@ -5,7 +5,7 @@ md`# Color Inspector`
 function _imgfile(Inputs){return(
 Inputs.file({
   label: "Upload image",
-  accept: ".png,.jpg",
+  accept: ".png,.jpg,.bmp,.gif",
   required: true
 })
 )}
@@ -14,8 +14,8 @@ async function _image(imgfile,DOM,$0)
 {
   let img = await imgfile.image();
   let aspect = img.width / img.height;
-  let height = 480;
-  let width = height * aspect;
+  let width = 720;
+  let height = width / aspect;
   let canvas = DOM.canvas(width, height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0, width, height);
